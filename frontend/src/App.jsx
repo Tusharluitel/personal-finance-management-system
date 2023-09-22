2d2092aa500c34633d042d2875763c76becafa9b
Tushar Luitel <tusharluitel88@gmail.com>
2023-09-22 17:23:27 +0545

    Chore: Added frontend login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
-
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <h1 className="text-5xl font-bold underline">Hello world!</h1>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Route index element={<Login />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
